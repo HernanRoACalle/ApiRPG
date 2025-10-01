33a1d35003b7a5f1e9918cc2ce75b14706e690c4
Etec Prof. Horácio Augusto da Silveira <discov126@gmail.com>
2025-10-01 19:32:38 -0300

    V5

---
+++ Migrations/20251001220457_MigracaoUsuario.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RpgApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracaoUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "FotoPersonagem",
+                table: "TB_PERSONAGENS",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId",
+                table: "TB_PERSONAGENS",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TB_USUARIOS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    Foto = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    Latitude = table.Column<double>(type: "float", nullable: true),
+                    Longitude = table.Column<double>(type: "float", nullable: true),
+                    DataAcesso = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Perfil = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_USUARIOS", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "TB_PERSONAGENS",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FotoPersonagem", "UsuarioId" },
+                values: new object[] { null, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "TB_PERSONAGENS",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FotoPersonagem", "UsuarioId" },
+                values: new object[] { null, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "TB_PERSONAGENS",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "FotoPersonagem", "UsuarioId" },
+                values: new object[] { null, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "TB_PERSONAGENS",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "FotoPersonagem", "UsuarioId" },
+                values: new object[] { null, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "TB_PERSONAGENS",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "FotoPersonagem", "UsuarioId" },
+                values: new object[] { null, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "TB_PERSONAGENS",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "FotoPersonagem", "UsuarioId" },
+                values: new object[] { null, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "TB_PERSONAGENS",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "FotoPersonagem", "UsuarioId" },
+                values: new object[] { null, 1 });
+
+            migrationBuilder.InsertData(
+                table: "TB_USUARIOS",
+                columns: new[] { "Id", "DataAcesso", "Email", "Foto", "Latitude", "Longitude", "PasswordHash", "PasswordSalt", "Perfil", "Username" },
+                values: new object[] { 1, null, "seuEmail@gmail.com", null, -23.520024100000001, -46.596497999999997, new byte[] { 99, 154, 177, 164, 4, 156, 71, 146, 179, 119, 43, 244, 84, 102, 229, 190, 212, 225, 103, 169, 215, 69, 30, 120, 134, 33, 174, 25, 251, 138, 25, 206, 14, 209, 153, 54, 209, 42, 208, 218, 16, 26, 78, 146, 89, 24, 245, 12, 251, 232, 236, 247, 175, 224, 172, 206, 105, 241, 190, 195, 76, 236, 182, 249 }, new byte[] { 92, 10, 159, 235, 112, 68, 125, 61, 44, 181, 38, 17, 71, 160, 243, 172, 138, 109, 204, 183, 12, 247, 122, 1, 236, 152, 94, 130, 32, 139, 125, 28, 217, 194, 229, 180, 101, 25, 234, 71, 70, 122, 152, 216, 167, 72, 233, 94, 160, 167, 177, 50, 249, 25, 130, 49, 83, 84, 10, 21, 120, 47, 144, 200, 243, 156, 224, 252, 185, 28, 52, 34, 180, 251, 72, 35, 71, 235, 105, 223, 212, 202, 138, 171, 141, 231, 210, 141, 104, 147, 245, 149, 150, 132, 66, 191, 113, 9, 201, 18, 65, 248, 230, 109, 195, 203, 216, 13, 235, 171, 123, 117, 118, 145, 5, 40, 28, 196, 170, 135, 35, 217, 159, 52, 109, 246, 187, 159 }, "Admin", "UsuarioAdmin" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TB_PERSONAGENS_UsuarioId",
+                table: "TB_PERSONAGENS",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TB_PERSONAGENS_TB_USUARIOS_UsuarioId",
+                table: "TB_PERSONAGENS",
+                column: "UsuarioId",
+                principalTable: "TB_USUARIOS",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TB_PERSONAGENS_TB_USUARIOS_UsuarioId",
+                table: "TB_PERSONAGENS");
+
+            migrationBuilder.DropTable(
+                name: "TB_USUARIOS");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TB_PERSONAGENS_UsuarioId",
+                table: "TB_PERSONAGENS");
+
+            migrationBuilder.DropColumn(
+                name: "FotoPersonagem",
+                table: "TB_PERSONAGENS");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "TB_PERSONAGENS");
+        }
+    }
+}
---
+++ Models/Personagem.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using RpgApi.Models.Enuns;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace RpgApi.Models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int PontosVida { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; }
+        public ClasseEnum Classe { get; set; }
+
+        public byte[]? FotoPersonagem { get; set; }
+        public int? UsuarioId { get; set; }
+        [JsonIgnore]
+        public Usuarios? Usuario { get; set;}
+        [JsonIgnore]
+        public Arma? Arma { get; set; }
+        public int Disputas { get; set; }
+        public int Vitorias { get; set; }
+        public int Derrotas { get; set; }
+    }
+}
